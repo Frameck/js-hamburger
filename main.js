69bbc9807308e51d41174f0dc93d07bdbd97ab83
Frameck <fra9879@gmail.com>
2021-10-11 21:05:17 +0200

    Completed exercise - optimized code

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const hambugerMenuClose = document.querySelector(".close")
 
 // opening the hamburger menu when clicking on the icon
 hambugerMenuIcon.addEventListener('click', function () {
-    hambugerMenu.style.display = 'block'
+    hambugerMenu.classList.add('active')
 })
 
 // close the hamburger menu when clicking on the 'X' icon
 hambugerMenuClose.addEventListener('click', function () {
-    hambugerMenu.style.display = 'none'
+    hambugerMenu.classList.remove('active')
 })
